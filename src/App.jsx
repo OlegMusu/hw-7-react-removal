d0da54a012d851f4f289b1a44a7f89ec8977525a
OlegMusu <175102757+OlegMusu@users.noreply.github.com>
2026-05-16 01:09:23 +0300

    hw-7 removal in react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Component } from 'react'
+import Tasks from './Components/TaskList/TaskList'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Tasks/>
+    </>
+  )
+}
+
+// class App extends Component{
+//   render(){
+//     return (
+//       <>
+
+//       </>
+//     )
+//   }
+// }
+
+export default App
---
+++ src/Components/TaskList/TaskList.jsx
@@ -0,0 +1,57 @@
+import { Component } from "react";
+import './TaskList.css'
+
+class TaskList extends Component {
+    state = {
+        tasks: [
+            {
+            id: 0, 
+            img: 'https://i.pinimg.com/736x/c0/8f/d8/c08fd819091cc1914a8fbff830c746d7.jpg',
+            }, 
+            {
+            id: 1, 
+            img: 'https://i.pinimg.com/736x/48/81/3a/48813a6550cc5fd0254ba6c61f2a80f3.jpg',
+            }, 
+            {
+            id: 2, 
+            img: 'https://i.pinimg.com/736x/f3/d0/c1/f3d0c1a6cbbb7e023d131987b177e6e4.jpg'
+            },
+            {
+            id: 3, 
+            img: 'https://i1-c.pinimg.com/736x/70/4c/ac/704cac241c6ab84d1fce9e4a76e00226.jpg'
+            },
+            {
+            id: 4, 
+            img: 'https://i1-c.pinimg.com/736x/d0/2b/35/d02b350bcc875cea28cf9e8379cd34b2.jpg'
+            },
+            {
+            id: 5, 
+            img: 'https://i.pinimg.com/736x/b0/59/4e/b0594e728183eadcaaf6c0fc85030326.jpg'
+            }
+        ],
+    }
+
+    hendleRemove = (taskId) => {
+        this.setState({
+            tasks: this.state.tasks.filter(({id}) => {
+                return id !== taskId
+            })
+        })
+    }
+
+  render() {
+
+    return (
+        <ul className="task-list">
+            {this.state.tasks.map(({id, img}) => (
+                <li key={id} className="task-item">
+                    <img className="task-image" src={img} alt="Task" />
+                    <button className="remove-button" type="button" onClick={() => this.hendleRemove(id)}>Remove</button>
+                </li>
+            ))}
+        </ul>
+    )
+  }
+}
+
+export default TaskList
